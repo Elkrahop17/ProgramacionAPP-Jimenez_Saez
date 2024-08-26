ceaecc87e56a7501227b5a493bb63eb876609aa4
Elkrahop17 <di.jimeneze@duocuc.cl>
2024-08-26 18:18:25 -0400

    se agrego pagina inicio, mas otros avances, para que lo vea el alexander.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,9 +7,17 @@ const routes: Routes = [
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
+    path: 'inicio',
+    loadChildren: () => import('./inicio/inicio.module').then(m => m.InicioPageModule)
+  },
+  {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },
+  {
+    path: 'inicio',
+    loadChildren: () => import('./inicio/inicio.module').then( m => m.InicioPageModule)
   },
 ];
 
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,47 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  username: string;
+  password: string;
+  constructor(private alertController: AlertController, private navCtrl: NavController) {
+    this.username = "";
+    this.password = "";
+  }
+  
+  async loginUsuario(event:Event){
+    event.preventDefault();
 
-  constructor() {}
+    // logica para verificar si las credenciales son correctas
+    if (this.username === 'usuario' && this.password === 'contraseña') {
+      await this.showAlert('Éxito', 'Inicio de sesión exitoso.');
+      console.log("Inicio de sesión exitoso.");
+
+      this.navCtrl.navigateForward('/inicio');
+      
+    } else {
+      await this.showAlert('Error', 'Credenciales incorrectas. Por favor, inténtalo de nuevo.');
+      console.log("Credenciales incorrectas.");
+    }
+
+  }
+
+
+  // Método para manejar el restablecimiento de contraseña
+  async resetPassword() {
+    await this.showAlert('Restablecer Contraseña', 'Se ha enviado un enlace para restablecer la contraseña a tu correo.');
+    console.log("Enlace de restablecimiento de contraseña enviado.");
+  }
+
+  //Metodo para mostrar una alerta
+  async showAlert(header: string, message: string) {
+    const alert = await this.alertController.create({
+      header: header,
+      message: message,
+      buttons: ['OK']
+    });
+
+    // Muestra la alerta en pantalla
+    await alert.present();
+  }
 
 }
