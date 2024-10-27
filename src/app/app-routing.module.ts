a73a884d56d758fe319528647b373c83e6ed2be2
alsaez97 <al.saezl@duocuc.cl>
2024-10-26 22:04:28 -0300

    Arreglo estilo not-found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -50,12 +50,16 @@ const routes: Routes = [
   {
     path: 'crear-usuario',
     loadChildren: () => import('./crear-usuario/crear-usuario.module').then( m => m.CrearUsuarioPageModule)
-  },  {
+  },
+  {
     path: 'not-found',
     loadChildren: () => import('./not-found/not-found.module').then( m => m.NotFoundPageModule)
   },
 
-
+  {
+    path: '**', // Ruta comodín para URLs no encontradas
+    redirectTo: 'not-found'
+  }
   
 
 
