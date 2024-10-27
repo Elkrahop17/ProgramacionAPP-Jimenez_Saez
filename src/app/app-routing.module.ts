65d8e8e922ed5214f13d577c3084804d038861ea
alsaez97 <al.saezl@duocuc.cl>
2024-10-26 21:48:33 -0300

    Creación rama 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -50,7 +50,13 @@ const routes: Routes = [
   {
     path: 'crear-usuario',
     loadChildren: () => import('./crear-usuario/crear-usuario.module').then( m => m.CrearUsuarioPageModule)
-  }
+  },  {
+    path: 'not-found',
+    loadChildren: () => import('./not-found/not-found.module').then( m => m.NotFoundPageModule)
+  },
+
+
+  
 
 
 ];
